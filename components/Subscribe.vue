e2130444f9d6c0a64b3bb2839669e3e1bedf66a3
xxx <huanghanxue@gxb.io>
2022-03-29 23:12:22 +0800

    chore: Modify the conflicting part

--- components/Subscribe.vue
+++ components/Subscribe.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-<<<<<<< Updated upstream
     <v-container class="mt-12">
       <h2>Subscribe</h2>
         <v-row class="mt-10" justify="space-between">
@@ -12,18 +11,7 @@
               <v-col
                 cols="12"
                 md="9"
-                
-=======
-    <v-container class="mt-12 align-item">
-        <v-row class="mt-10" justify="space-between">
-          <v-col cols="12" md="6" align-self="flex-end">
-            <h2>SubScribe</h2>
-            <div class="left-font">Subscribe ToThe Newsletter To Stay Up-To-Date With<br/>REI Network Updates And Events.</div>
-            <v-row justify="space-between" align="center" style="margin-top:20px">
-              <v-col
-                cols="12"
-                md="9"
->>>>>>> Stashed changes
+
               >
                 <v-text-field
                   v-model="email"
@@ -31,13 +19,7 @@
                   dense
                   hide-details
                   outlined
-<<<<<<< Updated upstream
                   placeholder="Your email address"
-=======
-                  placeholder="Placeholder"
-                  dense
-                  class="text-field"
->>>>>>> Stashed changes
                 ></v-text-field>
               </v-col>
                 <v-col cols="12" md="3">
@@ -118,7 +100,6 @@ export default {
   },
   data() {
     return {
-<<<<<<< Updated upstream
       email: '',
       loading: false,
       disabled: true,
@@ -130,101 +111,6 @@ export default {
       banner3: require('@/assets/img/ecosystem/Stablecoin.png'),
       banner4: require('@/assets/img/ecosystem/NFTMarketplace.png')
     }
-=======
-      email: "",
-      links: [
-        {
-          icon: "mdi-github",
-          href: "https://github.com/rei-network",
-          color: "#4b4b4b",
-        },
-        {
-          icon: "mdi-twitter",
-          href: "https://twitter.com/GXChainGlobal",
-          color: "#60aadb",
-        },
-        {
-          icon: "mdi-reddit",
-          href: "https://www.reddit.com/r/GXS/",
-          color: "#eb443b",
-        },
-        {
-          // href: "https://t.me/GXChain_international",
-          icon: "M9.78,18.65L10.06,14.42L17.74,7.5C18.08,7.19 17.67,7.04 17.22,7.31L7.74,13.3L3.64,12C2.76,11.75 2.75,11.14 3.84,10.7L19.81,4.54C20.54,4.21 21.24,4.72 20.96,5.84L18.24,18.65C18.05,19.56 17.5,19.78 16.74,19.36L12.6,16.3L10.61,18.23C10.38,18.46 10.19,18.65 9.78,18.65Z",
-          color: "#41a0d7",
-          subs: [
-            {
-              link: "https://t.me/GXChain_international",
-              en: "English International Community",
-            },
-            {
-              link: "https://t.me/gxchainkorean",
-              en: "Korean Community",
-            },
-            {
-              link: "https://t.me/GXChain_Rus",
-              en: "Russian Community",
-            },
-            {
-              link: "https://t.me/GXChainhispano",
-              en: "Spanish Community",
-            },
-            {
-              link: "https://t.me/GXChainDeutsch",
-              en: "German Community",
-            },
-            {
-              link: "https://t.me/GXChainJapanese",
-              en: "Japanese Community",
-            },
-            {
-              link: "https://t.me/GXChainBrazil",
-              en: "Brazil Community",
-            },
-            {
-              link: "https://t.me/GXChainItalia",
-              en: "Italian Community",
-            },
-            {
-              link: "https://t.me/gxchainvietnam",
-              en: "Vietnamese Community",
-            },
-            {
-              link: "https://t.me/GXChainAfrica",
-              en: "African Community",
-            },
-            {
-              link: "https://t.me/GXChainTurkey",
-              en: "Turkish Community",
-            },
-            {
-              link: "https://t.me/gxchainsrilanka",
-              en: "Sri Lanka Community",
-            },
-            {
-              link: "https://t.me/GXChain_Indonesia",
-              en: "Indonesian Community",
-            },
-          ],
-        },
-        // {
-        //   icon: "M12,0 C5.3671875,0 0,5.3671875 0,12 C0,18.6328125 5.3671875,24 12,24 C18.6328125,24 24,18.6328125 24,12 C24,5.3671875 18.6328125,0 12,0 Z M9.2953125,17.7140625 L9.2953125,17.7140625 L9.2953125,17.7140625 L5.1046875,15.6609375 C5.0625,15.61875 4.9734375,15.5296875 4.9734375,15.440625 L4.9734375,5.934375 L9.2484375,8.071875 L9.2484375,17.7140625 L9.2953125,17.7140625 Z M9.8203125,13.1765625 L9.8203125,8.859375 L13.659375,15.0984375 L9.8203125,13.1765625 Z M11.521875,10.6453125 L14.4890625,5.8875 L18.8109375,8.0671875 L14.3578125,15.2671875 L11.521875,10.6453125 Z M18.9375,17.7140625 L18.9375,17.7140625 L18.9375,17.7140625 L14.746875,15.6609375 L18.9375,8.859375 L18.9375,17.7140625 Z",
-        //   href: "https://4everland.medium.com/",
-        //   color: "#4b4b4b",
-        // },
-        {
-          img: "img/icon/kakao-talk.svg",
-          color: "#ffc107",
-          href: "https://open.kakao.com/o/gghzljsb",
-        },
-        {
-          icon: "mdi-discord",
-          href: "https://discord.com/invite/zhwWkXYtJt",
-          color: "#714bdf",
-        },
-      ],
-    };
->>>>>>> Stashed changes
   },
   methods: {
     async subscribe() {
