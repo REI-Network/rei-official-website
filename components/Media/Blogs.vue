ad84655ca7b80bbe196bcc4e3495d45daf12fd94
haifun <haifuncn@gmail.com>
2022-03-30 11:49:49 +0800

    chore: modify latest news

--- components/Media/Blogs.vue
+++ components/Media/Blogs.vue
@@ -35,6 +35,7 @@
                           width="120"
                           height="36"
                           :href="lists.length>0?lists[model].link:''"
+                          target="_blank"
                           tile
                         >
                           Read More
@@ -48,7 +49,10 @@
               <v-card class="right-content"  
                   v-for="(item,i) in otherLists"
                   :key="i"
-                  :src="item.img">
+                  :src="item.img"
+                  @click="goto(item.link)"
+                  >
+                  
                 <v-img :src="item.img" width="100%"  :aspect-ratio="16/9" />
                 <v-list two-line>
                   <v-list-item>
@@ -62,9 +66,9 @@
               
             </v-col>
           </v-row>
-          <h2>News</h2>
+          <h2 class="my-12">News</h2>
           <v-row>
-            <v-col cols="4" md="4" v-for="(item,i) in news">
+            <v-col cols="4" md="4" v-for="(item,i) in news" :key="i">
                 <div v-html="item"></div>
             </v-col>
           </v-row>
@@ -79,13 +83,12 @@ export default {
       model: 0,
       lists: [],
       otherLists:[],
-      news:['<blockquote class="twitter-tweet"><p lang="en" dir="ltr">We&#39;re happy to announce that <a href="https://twitter.com/search?q=%24REI&amp;src=ctag&amp;ref_src=twsrc%5Etfw">$REI</a> is listed on <a href="https://twitter.com/imTokenOfficial?ref_src=twsrc%5Etfw">@imTokenOfficial</a>- an easy and secure digital wallet!ðŸ›¡ï¸<br><br>Comment to let us know which wallet you most want to support <a href="https://twitter.com/search?q=%24REI&amp;src=ctag&amp;ref_src=twsrc%5Etfw">$REI</a> Token. We will reach out to the team for the listingðŸ›©<a href="https://twitter.com/hashtag/imtoken?src=hash&amp;ref_src=twsrc%5Etfw">#imtoken</a> <a href="https://twitter.com/hashtag/GXChain?src=hash&amp;ref_src=twsrc%5Etfw">#GXChain</a> <a href="https://twitter.com/hashtag/EVM?src=hash&amp;ref_src=twsrc%5Etfw">#EVM</a> <a href="https://twitter.com/hashtag/Crypto?src=hash&amp;ref_src=twsrc%5Etfw">#Crypto</a> <a href="https://twitter.com/hashtag/REINetwork?src=hash&amp;ref_src=twsrc%5Etfw">#REINetwork</a> <a href="https://twitter.com/hashtag/Blockchain?src=hash&amp;ref_src=twsrc%5Etfw">#Blockchain</a> <a href="https://t.co/G4MidkGaug">pic.twitter.com/G4MidkGaug</a></p>&mdash; REI Networkï½œFrom #GXChain! (@GXChainGlobal) <a href="https://twitter.com/GXChainGlobal/status/1501457225444777990?ref_src=twsrc%5Etfw">March 9, 2022</a></blockquote>',
-'<blockquote class="twitter-tweet"><p lang="en" dir="ltr"><a href="https://twitter.com/hashtag/REINetwork?src=hash&amp;ref_src=twsrc%5Etfw">#REINetwork</a> is delighted to announce the partnership with <a href="https://twitter.com/SupraOracles?ref_src=twsrc%5Etfw">@SupraOracles</a> ðŸ”¥<br><br>SupraOracles strives to bridge the gap between traditional capital markets and the <a href="https://twitter.com/hashtag/Web3?src=hash&amp;ref_src=twsrc%5Etfw">#Web3</a> ecosystem.<br><br>Letâ€™s get superb with Supra!ðŸ¦¸<a href="https://twitter.com/hashtag/oracle?src=hash&amp;ref_src=twsrc%5Etfw">#oracle</a> <a href="https://twitter.com/hashtag/GXChain?src=hash&amp;ref_src=twsrc%5Etfw">#GXChain</a> <a href="https://twitter.com/hashtag/EVM?src=hash&amp;ref_src=twsrc%5Etfw">#EVM</a> <a href="https://twitter.com/hashtag/Crypto?src=hash&amp;ref_src=twsrc%5Etfw">#Crypto</a> <br>More details: <a href="https://t.co/7IrDf2rtLz">https://t.co/7IrDf2rtLz</a></p>&mdash; REI Networkï½œFrom #GXChain! (@GXChainGlobal) <a href="https://twitter.com/GXChainGlobal/status/1501393418102067206?ref_src=twsrc%5Etfw">March 9, 2022</a></blockquote>',
-'<blockquote class="twitter-tweet"><p lang="en" dir="ltr">ðŸŽ‰ <a href="https://twitter.com/hashtag/REINetwork?src=hash&amp;ref_src=twsrc%5Etfw">#REINetwork</a> Content Bounty Program is up and running! ðŸŽ‰<br><br>ðŸ’° Create content to talk about <a href="https://twitter.com/hashtag/REINetwork?src=hash&amp;ref_src=twsrc%5Etfw">#REINetwork</a>, let&#39;s spread the word out and get rewarded with juicy <a href="https://twitter.com/search?q=%24REI&amp;src=ctag&amp;ref_src=twsrc%5Etfw">$REI</a> rewards! ðŸ’°<br><br>Send your submission here: <a href="https://t.co/SjIHrTO8j2">https://t.co/SjIHrTO8j2</a><a href="https://twitter.com/hashtag/GXChain?src=hash&amp;ref_src=twsrc%5Etfw">#GXChain</a> <a href="https://twitter.com/hashtag/bounty?src=hash&amp;ref_src=twsrc%5Etfw">#bounty</a> <a href="https://twitter.com/hashtag/Crypto?src=hash&amp;ref_src=twsrc%5Etfw">#Crypto</a><a href="https://t.co/YfCldTl2SG">https://t.co/YfCldTl2SG</a></p>&mdash; REI Networkï½œFrom #GXChain! (@GXChainGlobal) <a href="https://twitter.com/GXChainGlobal/status/1500774283223580673?ref_src=twsrc%5Etfw">March 7, 2022</a></blockquote>']
+      news:[]
     }
   },
   mounted() {
     this.getDataList();
+    this.getLatestNews();
   },
   methods:{
       async getDataList(){
@@ -94,14 +97,30 @@ export default {
             console.log(data)
             this.setData(data.rows)
         } catch (error) {
-            //
+            console.log(error)
+        }
+      },
+      async getLatestNews(){
+          try {
+            const { data } = await this.$axios.get('/api/twitter/list')
+            if(data.rows.length>0){
+                let list = data.rows
+                this.news = list.map((item) =>{
+                    return item.json.replace(/<script.*<\/script>/gm, "")
+                });
+            }
+        } catch (error) {
+            console.log(error)
         }
       },
       setData(data){
           let _data = [].concat(data);
           this.lists = _data.slice(0,4);
           this.otherLists = _data.slice(4);
-      }
+      },
+      goto(href) {
+        window.open(href)
+      },
   },
   components: {
 
