8f042c010093aaddc998438e0c8accfd4dd1d008
xxx <huanghanxue@gxb.io>
2022-09-27 18:16:56 +0800

    chore: update index and ecosystem

--- components/IndexPage/BuiltDapps.vue
+++ components/IndexPage/BuiltDapps.vue
@@ -13,13 +13,113 @@
             solution.
           </div>
           <v-btn class="more-btn">
-            <a href="https://dao.rei.network/#/stake" target="_blank"
-              >learn more</a
-            ></v-btn
-          >
+            <a href="" target="_blank">learn more</a>
+          </v-btn>
         </v-col>
         <v-col cols="12" sm="6" class="all-dapps">
-          <div class="explore"></div>
+          <div class="explore">
+            <v-btn class="explore-btn">
+            <a href="" target="_blank">Explore all dApps</a>
+          </v-btn>
+          </div>
+          <v-row class="logo-two">
+            <v-col>
+              <div class="img-back">
+                <v-img class="image" :src="logo1" width="80" height="80" />
+              </div>
+            </v-col>
+            <v-col>
+              <div class="img-back">
+                <v-img class="image" :src="logo2" width="80" height="80" />
+              </div>
+            </v-col>
+            <v-col>
+              <div class="img-back">
+                <v-img class="image" :src="logo3" width="80" height="80" />
+              </div>
+            </v-col>
+            <v-col>
+              <div class="img-back">
+                <v-img class="image" :src="logo4" width="80" height="80" />
+              </div>
+            </v-col>
+          </v-row>
+          <v-row justify="space-around">
+            <v-col>
+              <div class="img-back">
+                <v-img class="image" :src="logo5" width="80" height="80" />
+              </div>
+            </v-col>
+            <v-col>
+              <div class="img-back">
+                <v-img class="image" :src="logo6" width="80" height="80" />
+              </div>
+            </v-col>
+            <v-col>
+              <div class="img-back">
+                <v-img class="image" :src="logo7" width="80" height="80" />
+              </div>
+            </v-col>
+            <v-col>
+              <div class="img-back">
+                <v-img class="image" :src="logo8" width="80" height="80" />
+              </div>
+            </v-col>
+            <v-col>
+              <div class="img-back">
+                <v-img class="image" :src="logo1" width="80" height="80" />
+              </div>
+            </v-col>
+          </v-row>
+           <v-row class="logo-two">
+            <v-col>
+              <div class="img-back">
+                <v-img class="image" :src="logo1" width="80" height="80" />
+              </div>
+            </v-col>
+            <v-col>
+              <div class="img-back">
+                <v-img class="image" :src="logo2" width="80" height="80" />
+              </div>
+            </v-col>
+            <v-col>
+              <div class="img-back">
+                <v-img class="image" :src="logo3" width="80" height="80" />
+              </div>
+            </v-col>
+            <v-col>
+              <div class="img-back">
+                <v-img class="image" :src="logo4" width="80" height="80" />
+              </div>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col>
+              <div class="img-back">
+                <v-img class="image" :src="logo5" width="80" height="80" />
+              </div>
+            </v-col>
+            <v-col>
+              <div class="img-back">
+                <v-img class="image" :src="logo6" width="80" height="80" />
+              </div>
+            </v-col>
+            <v-col>
+              <div class="img-back">
+                <v-img class="image" :src="logo7" width="80" height="80" />
+              </div>
+            </v-col>
+            <v-col>
+              <div class="img-back">
+                <v-img class="image" :src="logo8" width="80" height="80" />
+              </div>
+            </v-col>
+            <v-col>
+              <div class="img-back">
+                <v-img class="image" :src="logo1" width="80" height="80" />
+              </div>
+            </v-col>
+          </v-row>
         </v-col>
       </v-row>
     </v-container>
@@ -29,7 +129,16 @@
 export default {
   name: "",
   data() {
-    return {};
+    return {
+      logo1: require("../../assets/img/logo/logo_1.jpg"),
+      logo2: require("../../assets/img/logo/logo_2.jpg"),
+      logo3: require("../../assets/img/logo/logo_3.jpg"),
+      logo4: require("../../assets/img/logo/logo_4.png"),
+      logo5: require("../../assets/img/logo/logo_5.png"),
+      logo6: require("../../assets/img/logo/logo_6.png"),
+      logo7: require("../../assets/img/logo/logo_7.jpeg"),
+      logo8: require("../../assets/img/logo/logo_8.jpeg"),
+    };
   },
   components: {},
   watch: {},
@@ -74,18 +183,41 @@ a {
   border-radius: 8px;
   background-color: #fff;
   padding: 20px;
+  position: relative;
   .explore {
-    width: 270px;
-    height: 70px;
-    // background: #1b11f7;
+    // width: 270px;
+    // height: 70px;
     border-radius: 35px;
-    border: 35px solid;
-    border-image: linear-gradient(
-        171deg,
-        rgba(255, 209, 209, 0.5),
-        rgba(27, 17, 247, 0.06)
-      )
-      35 35;
+    background: linear-gradient(171deg,rgba(255, 209, 209, 0.5),rgba(239,237,255));
+    position: absolute;
+    top: 148px;
+    left: 160px;
+    z-index: 9;
+  }
+  .explore-btn {
+    background-color: blue;
+    color: #fff;
+    border-radius: 20px;
+    padding: 20px;
+    margin: 20px 28px;
+  }
+  .logo-two{
+    .img-back{
+      margin:0 15px;
+    }
+  }
+  .img-back {
+    width: 80px;
+    height: 80px;
+    border-radius: 50px;
+    box-shadow: 0 0 20px #ddd;
+    .image {
+      border-radius: 50px;
+    }
+  }
+  .image:hover{
+    border: 1px blue solid;
+    cursor: pointer;
   }
 }
 </style>
--- components/ReiUse/GameFi.vue
+++ components/ReiUse/GameFi.vue
@@ -1,43 +1,79 @@
 <template>
   <div>
     <v-container class="mt-12 align-item">
-          <h1>Projects</h1>
-          <v-row class="mt-8">
-            <v-col
-              cols="12"
-              sm="4"
-              class="project-background"
-              v-for="(item,i) in lists"
-              :key="i"
-            >
-            <!-- v-for="(item,i) in Lists"
+      <h1>Projects</h1>
+      <!-- <v-tabs hide-slider>
+        <v-chip-group active-class="chip_group" v-model="intervalModel" mandatory>
+        <v-tab>
+          <v-chip class="ma-2"> All </v-chip>
+        </v-tab>
+        <v-tab>
+          <v-chip class="ma-2"> NFT </v-chip>
+        </v-tab>
+        <v-tab>
+          <v-chip class="ma-2"> GameFi </v-chip>
+        </v-tab>
+        <v-tab>
+          <v-chip class="ma-2"> DeFi </v-chip>
+        </v-tab>
+        </v-chip-group>
+      </v-tabs> -->
+      <v-row align="center">
+        <v-chip-group
+          active-class="chip_group"
+          v-model="intervalModel"
+          mandatory
+        >
+          <v-chip class="ma-2" x-small v-for="tag in tags" :key="tag.query" @click="getList(tag)">
+            {{ tag.title }}
+          </v-chip>
+        </v-chip-group>
+      </v-row>
+      <v-row class="mt-8">
+        <v-col
+          cols="12"
+          sm="4"
+          class="project-background"
+          v-for="(item, i) in lists"
+          :key="i"
+        >
+          <!-- v-for="(item,i) in Lists"
               :key="i" -->
-              <v-hover
-                v-slot="{ hover }"
-                open-delay="200">
-                <v-card
-                  elevation="0"
-                  :class="{ 'on-hover': hover }"
-                  @click="goto(item.app_url)"
-                  >
-                  <v-list two-line class="list-item">
-                    <v-list-item>
-                      <v-list-item-avatar size="80">
-                        <v-img :src="item.logo"></v-img>
-                      </v-list-item-avatar>
-                      <v-list-item-content>
-                        <v-list-item-title class="font-white"><h3>{{item.app_name}}</h3></v-list-item-title>
-                        <v-list-item-subtitle class="subtitle-content font-white">{{item.description}}</v-list-item-subtitle>
-                        <v-list-item-icon class="right-icon" style="margin-bottom:0;">
-                          <img src='../../assets/img/arrow.png' width="20" height="6"/>
-                        </v-list-item-icon>
-                      </v-list-item-content>
-                    </v-list-item>
-                  </v-list>
-                </v-card>
-            </v-hover>
-            </v-col>
-          </v-row>
+          <v-hover v-slot="{ hover }" open-delay="200">
+            <v-card
+              elevation="0"
+              :class="{ 'on-hover': hover }"
+              @click="goto(item.app_url)"
+            >
+              <v-list two-line class="list-item">
+                <v-list-item>
+                  <v-list-item-avatar size="80">
+                    <v-img :src="item.logo"></v-img>
+                  </v-list-item-avatar>
+                  <v-list-item-content>
+                    <v-list-item-title class="font-white"
+                      ><h3>{{ item.app_name }}</h3></v-list-item-title
+                    >
+                    <v-list-item-subtitle class="subtitle-content font-white">{{
+                      item.description
+                    }}</v-list-item-subtitle>
+                    <v-list-item-icon
+                      class="right-icon"
+                      style="margin-bottom: 0"
+                    >
+                      <img
+                        src="../../assets/img/arrow.png"
+                        width="20"
+                        height="6"
+                      />
+                    </v-list-item-icon>
+                  </v-list-item-content>
+                </v-list-item>
+              </v-list>
+            </v-card>
+          </v-hover>
+        </v-col>
+      </v-row>
     </v-container>
   </div>
 </template>
@@ -45,60 +81,98 @@
 export default {
   data() {
     return {
-      banner: require('@/assets/img/ecosystem/exchange.png'),
-      banner1: require('@/assets/img/ecosystem/Lending.png'),
-      banner2: require('@/assets/img/ecosystem/REIiconGame.png'),
-      banner3: require('@/assets/img/ecosystem/Stablecoin.png'),
-      banner4: require('@/assets/img/ecosystem/NFTMarketplace.png'),
-      lists: []
-    }
+      intervalModel: 0,
+      lists: [],
+      // tags: ["All", "NFT", "GameFi", "DeFi"],
+      tags: [
+        {
+          query:"",
+          title:"All",
+        },
+        {
+          query:"NFT",
+          title:"NFT",
+        },
+        {
+          query:"GameFi",
+          title:"GameFi",
+        },
+        {
+          query:"DeFi",
+          title:"DeFi",
+        }
+      ]
+    };
   },
   mounted() {
     this.getDataList();
   },
-  methods:{
-      async getDataList(){
-        try {
-            const { data } = await this.$axios.get('/api/project/list')
-            this.setData(data.rows)
-        } catch (error) {
-            //
-        }
-      },
-      setData(data){
-          this.lists = data;
-      },
-      goto(href) {
-        window.open(href)
-      },
+  methods: {
+    async getDataList() {
+      try {
+        const { data } = await this.$axios.get("/api/project/list");
+        this.setData(data.rows);
+      } catch (error) {
+        //
+      }
+    },
+    async getList(value){
+      try {
+        const { data } = await this.$axios.get(`/api/project/list?type=${value.query}`);
+        this.setData(data.rows);
+        console.log(value)
+      } catch (error) {
+        //
+      }
+    },
+    setData(data) {
+      this.lists = data;
+    },
+    goto(href) {
+      window.open(href);
+    },
   },
-  components: {
-
-  },
-}
+  components: {},
+};
 </script>
 
 <style lang="scss" scoped>
-.align-item{
+.align-item {
   max-width: 1220px;
 }
-.subtitle-content{
+.subtitle-content {
   margin-top: 12px;
 }
-.right-icon{
-  display:flex;
-  justify-content:flex-end;
-  padding-right:30px
+.right-icon {
+  display: flex;
+  justify-content: flex-end;
+  padding-right: 30px;
 }
-.list-item{
-  background-color:#EEF3F7;
+.list-item {
+  background-color: #eef3f7;
 }
-.list-item:hover{
+.list-item:hover {
   box-shadow: 0 0 20px #ddd !important;
   transform: translateY(-10px);
-  transition: transform .8s;
-  .font-white{
+  transition: transform 0.8s;
+  .font-white {
     // color: #FFF;
   }
+}
+.v-chip:not(.v-chip--active) {
+  padding: 14px 40px;
+  background-color: #f1f5f8;
+  border-radius: 20px;
+  text-transform: none !important;
+}
+.v-chip.v-chip--active {
+  padding: 14px 40px;
+  background-color: red;
+  border-radius: 20px;
+  text-transform: none !important;
+}
+.v-application .chip_group {
+  background-color: #1d10f8 !important;
+  color: #fff;
 }
 </style>
